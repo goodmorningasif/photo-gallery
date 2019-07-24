bf8366c543808bfb7455ab923b02ce84ea3742a7
aahmed <aahmed@shapesecurity.com>
2019-07-24 09:01:19 -0700

    add scrolling logic

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,19 +5,51 @@ import React from "react";
 import { IMAGES } from "../enums";
 
 class AppComponent extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      pictureIndx: 0,
+      totalImages: IMAGES.length
+    };
+  }
+
+  handleClick = type => {
+    const { pictureIndx, totalImages } = this.state;
+    let newIndx;
+    if (type === "left") {
+      newIndx = pictureIndx - 1 < 0 ? totalImages - 1 : pictureIndx - 1;
+    }
+    if (type === "right") {
+      newIndx = pictureIndx + 1 > totalImages - 1 ? 0 : pictureIndx + 1;
+    }
+    this.setState(() => ({ pictureIndx: newIndx }));
+  };
+
   render() {
+    const { pictureIndx } = this.state;
+    const { handleClick } = this;
+
+    console.log("+", pictureIndx);
     return (
       <div>
-        <div className="galleryWrapper">
+        <div
+          className="galleryWrapper"
+          style={{ transform: `translateX(-${pictureIndx}00vw)` }}
+        >
           {IMAGES.map(image => {
             return (
-              <div className="pictureWrapper">
-                <img src={image.path} alt={image.alt} />
+              <div className="pictureWrapper" key={image.title}>
+                <div
+                  className="picture"
+                  style={{ backgroundImage: `url(${image.path})` }}
+                />
                 <span>{image.caption}</span>
               </div>
             );
           })}
         </div>
+        <div className="leftButton" onClick={() => handleClick("left")} />
+        <div className="rightButton" onClick={() => handleClick("right")} />
       </div>
     );
   }
--- src/enums.js
+++ src/enums.js
@@ -2,21 +2,21 @@ export const IMAGES = [
   {
     path: "./images/dave-mullen-5jA1rVTdx0M-unsplash.jpg",
     caption: "Photo by Dave Mullen",
-    alt: "a photo of concrete"
+    title: "DM"
   },
   {
     path: "./images/mak-m8ltmpRIVr0-unsplash.jpg",
     caption: "Photo by Mak",
-    alt: "a photo of concrete"
+    title: "M"
   },
   {
-    path: "./images/richard-ludwig-ZfebvEPaR2o-unsplash.jpg",
+    path: "./images/richard-ludwig-ZfebvEPaE2o-unsplash.jpg",
     caption: "Photo by Richard Ludwig",
-    alt: "a photo of concrete"
+    title: "RL"
   },
   {
     path: "./images/yann-allegre-7ezHwUsLGAQ-unsplash.jpg",
     caption: "Photo by Yann Allegre",
-    alt: "a photo of concrete"
+    title: "YA"
   }
 ];
